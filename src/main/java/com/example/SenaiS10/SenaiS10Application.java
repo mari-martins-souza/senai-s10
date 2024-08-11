7cefb4ff2b591599e69814068011df2e546d8b8d
Mariana Martins <marih_ms@hotmail.com>
2024-08-11 20:14:08 -0300

    firts commit

---
+++ src/main/java/com/example/SenaiS10/SenaiS10Application.java
@@ -0,0 +1,13 @@
+package com.example.SenaiS10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenaiS10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenaiS10Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SenaiS10/SenaiS10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SenaiS10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenaiS10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
